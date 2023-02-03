7dbf2d36b923dbfa49d36953a24c6bdea0c917f2
codenathan4 <96376114+codenathan4@users.noreply.github.com>
2023-02-03 23:32:26 +0300

    Update random pick with weight.py

--- random pick with weight.py
+++ random pick with weight.py
@@ -1,1 +1,11 @@
+class Solution:
+    def __init__(self, w: List[int]):
+        self.l = []
+        self.wt = 0
+        
+        for i in w:
+            self.wt += i
+            self.l.append(self.wt)
 
+    def pickIndex(self) -> int:
+        return bisect.bisect(self.l, random.randrange(self.wt))
