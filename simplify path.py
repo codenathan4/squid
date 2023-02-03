23591dd4f11f254d622a2c1ed0d74f525d59c783
codenathan4 <96376114+codenathan4@users.noreply.github.com>
2023-02-03 23:07:30 +0300

    Update simplify path.py

--- simplify path.py
+++ simplify path.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        for i in path.split("/"):
+            if i == "" or i == ".":
+                continue
+            elif i == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(i)
+                
+        return "/" + "/".join(stack)
