75c5192712b464aecf999318b275f5d9508107e7
codenathan4 <96376114+codenathan4@users.noreply.github.com>
2023-02-06 23:07:45 +0300

    Update race car.py

--- race car.py
+++ race car.py
@@ -1,1 +1,23 @@
+class Solution:
+    def racecar(self, target: int) -> int:
+        q = collections.deque([(0,1)])
+        out = 0
+        
+        while q:
+            l = len(q)
+            while l>0:
+                p, s = q.popleft()
 
+                if p == target:
+                    return out
+
+                q.append((p+s,s*2))
+
+                if (p+s > target and s > 0) or (p+s < target and s < 0):
+                    q.append((p,-1 if s>0 else 1))
+                
+                l -= 1
+            
+            out += 1
+            
+        return out
