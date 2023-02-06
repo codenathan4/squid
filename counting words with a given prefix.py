167ff4c1d097888026ed41623901d734d4ed7f36
codenathan4 <96376114+codenathan4@users.noreply.github.com>
2023-02-06 23:00:05 +0300

    Update counting words with a given prefix.py

--- counting words with a given prefix.py
+++ counting words with a given prefix.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        out = 0
+        for i in words:
+            if len(pref) <= len(i) and i[:len(pref)] == pref:
+                out += 1
+                
+        return out
