e4aa584d7d9708fe48a88683e8c838e617b396d8
codenathan4 <96376114+codenathan4@users.noreply.github.com>
2023-02-06 23:06:15 +0300

    Update longest string chain.py

--- longest string chain.py
+++ longest string chain.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        rec = {}
+        out = 0
+        
+        for i in sorted(words,key=len):
+            rec[i] = 1
+            
+            for j in range(len(i)):
+                h = i[:j] + i[j+1:]
+                
+                if h in rec:
+                    rec[i] = max(rec[h] + 1,rec[i])
+            
+            out = max(out,rec[i])
+        return out
