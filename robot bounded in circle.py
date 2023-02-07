a29e2eef14d2577d6b95c8bb92f3b65521bdbafe
codenathan4 <96376114+codenathan4@users.noreply.github.com>
2023-02-07 23:47:34 +0300

    Update robot bounded in circle.py

--- robot bounded in circle.py
+++ robot bounded in circle.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        position = 0
+        direction = 0
+        
+        for i in instructions:
+            if i == 'L':
+                position *= 1j
+                direction += 1
+            elif i == 'R':
+                position *= -1j
+                direction -= 1
+            else:
+                position += 1   
+          
+        if direction%4 != 0 or position == 0:
+            return True
