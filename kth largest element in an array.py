53701353e8418de2c96206db1da19a3071e1c814
codenathan4 <96376114+codenathan4@users.noreply.github.com>
2023-02-07 23:45:18 +0300

    Update kth largest element in an array.py

--- kth largest element in an array.py
+++ kth largest element in an array.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    import heapq
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq._heapify_max(nums)
+        for _ in range(k-1):
+            heapq._heappop_max(nums)
+        return heapq._heappop_max(nums)  
