59b95402ad4ccb07433fd16389c656a3b67654da
codenathan4 <96376114+codenathan4@users.noreply.github.com>
2023-02-09 23:56:06 +0300

    Update subarrays with k different integers.py

--- subarrays with k different integers.py
+++ subarrays with k different integers.py
@@ -1,1 +1,20 @@
-
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        def findAtMostK(nums,d):
+            rec = defaultdict(int);
+            ans=s=ct=0
+            for i in range(len(nums)):
+                if rec[nums[i]]==0:
+                    rec[nums[i]] += 1
+                    ct += 1
+                while ct > d:                
+                    if rec[nums[s]] - 1 == 1:
+                        rec[nums[i]] -= 1
+                        s += 1
+                        ct -= 1
+              
+                ans += i-s
+          
+            return ans
+        
+        return findAtMostK(nums,k) - findAtMostK(nums,k-1)
