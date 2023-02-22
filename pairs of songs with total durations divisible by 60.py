8d80563c6963b05d91270aedfa4b95fa0ec15e8b
codenathan4 <96376114+codenathan4@users.noreply.github.com>
2023-02-22 23:36:08 +0300

    Update pairs of songs with total durations divisible by 60.py

--- pairs of songs with total durations divisible by 60.py
+++ pairs of songs with total durations divisible by 60.py
@@ -1,1 +1,14 @@
 
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        rec = {}
+        out = 0
+        for i in time:
+            if (60-i%60)%60 in rec:
+                out += rec[(60 - i%60)%60]
+            if i%60 in rec:
+                rec[i%60] += 1
+            else:
+                rec[i%60] = 1
+        
+        return out
